91eaeb353bce9c1f2c86454b5014b9a61fa057ed
Focusmyme <146586311+Focusmyme@users.noreply.github.com>
2024-07-09 23:32:19 -0300

    primeira edição

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function clicar() {
+
+let valor = document.getElementById('inome').value
+let nomes = window.document.getElementById('nomes')
+
+
+nomes.innerText = `Olá ${valor}, Seja bem Vindo!`
+
+}
